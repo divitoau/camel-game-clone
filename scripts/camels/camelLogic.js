dde9d907338b6f70c6454852255813d3bc28c961
divitoau <divitoau@gmail.com>
2024-05-16 04:33:06 -0400

    added verticality to positioning and some reformatting

--- scripts/camels/camelLogic.js
+++ scripts/camels/camelLogic.js
@@ -1,45 +1,63 @@
 class Camel {
-  constructor(color, camelUnder, position) {
+  constructor(color, position, elevation, camelUnder) {
     this.color = color;
-    this.camelUnder = camelUnder;
     this.position = position;
+    this.elevation = elevation;
+    this.camelUnder = camelUnder;
   }
+
   move(rollNumber) {
     this.position += rollNumber;
   }
+
+  setStartingPosition(position, elevation, camelUnder) {
+    this.position = position;
+    this.elevation = elevation;
+    this.camelUnder = camelUnder;
+  }
 }
 
-const allCamels = [];
-
-allCamels.push(new Camel("blue", null, null));
-allCamels.push(new Camel("yellow", null, null));
-allCamels.push(new Camel("green", null, null));
-allCamels.push(new Camel("red", null, null));
-allCamels.push(new Camel("purple", null, null));
-allCamels.push(new Camel("white", null, null));
-allCamels.push(new Camel("black", null, null));
+const allCamels = [
+  new Camel("blue"),
+  new Camel("yellow"),
+  new Camel("green"),
+  new Camel("red"),
+  new Camel("purple"),
+  new Camel("white"),
+  new Camel("black"),
+];
 
 const setStartingPositions = () => {
   for (let i = 0; i < 6; i++) {
     let spawnRoll = rollDie();
     if (!spawnRoll.color.startsWith("grey-")) {
-      console.log(spawnRoll);
       const camel = allCamels.find((c) => c.color === spawnRoll.color);
-      if (camel) camel.position = spawnRoll.number;
+      const camelsOnSpace = allCamels.filter(
+        (c) => c.position === spawnRoll.number
+      );
+      camel.setStartingPosition(
+        spawnRoll.number,
+        camelsOnSpace.length,
+        camelsOnSpace.find((c) => c.elevation === camelsOnSpace.length - 1)
+      );
     }
   }
-    
+
   const crazyRoll = selectFace();
   const crazyNumber = crazyRoll > 3 ? crazyRoll - 3 : crazyRoll;
   const crazyColor = crazyRoll > 3 ? "black" : "white";
   const crazyCamel = allCamels.find((c) => c.color === crazyColor);
   const otherCrazyColor = crazyColor === "white" ? "black" : "white";
   const otherCrazyCamel = allCamels.find((c) => c.color === otherCrazyColor);
+  const occPosition = 17 - Math.ceil(Math.random() * 3);
 
-  crazyCamel.position = 17 - crazyNumber;
-  otherCrazyCamel.position = 17 - Math.ceil(Math.random() * 3);
-  console.log(crazyCamel);
-  console.log(otherCrazyCamel);
+  crazyCamel.setStartingPosition(17 - crazyNumber, 0);
+  otherCrazyCamel.setStartingPosition(
+    occPosition,
+    occPosition === crazyCamel.position ? 1 : 0,
+    occPosition === crazyCamel.position ? crazyCamel : undefined
+  );
 };
 
 setStartingPositions();
+allCamels.forEach((camel) => console.log(camel));
