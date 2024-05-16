982d9522e537a7dd8f8340eb48c3154555695137
divitoau <divitoau@gmail.com>
2024-05-16 00:47:42 -0400

    mild tidying

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const dice_button = document.querySelector(".dice_button");
-
 const allDice = ["blue", "yellow", "green", "red", "purple", "grey"];
-
 let diceInPyramid = [...allDice];
 
 const selectDie = () => {
@@ -19,34 +16,21 @@ const resolveResult = (die, face) => {
   let color = die;
   let number = face > 3 ? face - 3 : face;
   if (die === "grey") {
-    color = face > 3 ? "grey_black" : "grey_white";
+    color = face > 3 ? "grey-black" : "grey-white";
   }
-  const result = {
-    color,
-    number,
-  };
-  return result;
+  return { color, number };
 };
 
 const displayDie = (result) => {
   const nextTentNumber = 6 - diceInPyramid.length;
-  let nextTent = document.getElementById(`tent_${nextTentNumber}`);
-  nextTent.classList.add(`${result.color}_die`);
+  let nextTent = document.getElementById(`tent-${nextTentNumber}`);
+  nextTent.className = `dice-tent ${result.color}-die`;
   nextTent.innerText = result.number;
 };
 
 const resetPyramid = () => {
-  let allTents = document.querySelectorAll(".dice_tent");
-  allTents.forEach((tent) => {
-    tent.classList.remove(
-      "blue_die",
-      "yellow_die",
-      "green_die",
-      "red_die",
-      "purple_die",
-      "grey_black_die",
-      "grey_white_die"
-    );
+  document.querySelectorAll(".dice-tent").forEach((tent) => {
+    tent.className = "dice-tent";
     tent.innerText = "";
   });
   diceInPyramid = [...allDice];
@@ -57,7 +41,6 @@ const bopPyramid = () => {
     const selectedDie = selectDie();
     const selectedFace = selectFace();
     const result = resolveResult(selectedDie, selectedFace);
-    console.log(result);
     displayDie(result);
   } else {
     resetPyramid();
