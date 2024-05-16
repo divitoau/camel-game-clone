e7b221e2f04a29ecb2fed2d8d20fbb60a61e27b3
divitoau <divitoau@gmail.com>
2024-05-16 00:25:45 -0400

    made the rolled dice show on screen

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const dice_button = document.querySelector(".dice_button");
 
 const allDice = ["blue", "yellow", "green", "red", "purple", "grey"];
 
-let diceInPyramid = allDice;
+let diceInPyramid = [...allDice];
 
 const selectDie = () => {
   const dieIndex = Math.floor(Math.random() * diceInPyramid.length);
@@ -15,20 +15,52 @@ const selectFace = () => {
   return Math.ceil(Math.random() * 6);
 };
 
-const bopPyramid = () => {
-  const selectedDie = selectDie();
-  const selectedFace = selectFace();
-  let color = selectedDie;
-  let number = selectedFace > 3 ? selectedFace - 3 : selectedFace;
-
-  if (selectedDie === "grey") {
-    color = selectedFace > 3 ? "black" : "white";
+const resolveResult = (die, face) => {
+  let color = die;
+  let number = face > 3 ? face - 3 : face;
+  if (die === "grey") {
+    color = face > 3 ? "grey_black" : "grey_white";
   }
-
   const result = {
     color,
     number,
   };
+  return result;
+};
 
-  console.log(result);
+const displayDie = (result) => {
+  const nextTentNumber = 6 - diceInPyramid.length;
+  let nextTent = document.getElementById(`tent_${nextTentNumber}`);
+  nextTent.classList.add(`${result.color}_die`);
+  nextTent.innerText = result.number;
+};
+
+const resetPyramid = () => {
+  let allTents = document.querySelectorAll(".dice_tent");
+  allTents.forEach((tent) => {
+    tent.classList.remove(
+      "blue_die",
+      "yellow_die",
+      "green_die",
+      "red_die",
+      "purple_die",
+      "grey_black_die",
+      "grey_white_die"
+    );
+    tent.innerText = "";
+  });
+  diceInPyramid = [...allDice];
+};
+
+const bopPyramid = () => {
+  if (diceInPyramid.length > 1) {
+    const selectedDie = selectDie();
+    const selectedFace = selectFace();
+    const result = resolveResult(selectedDie, selectedFace);
+    console.log(result);
+    displayDie(result);
+  } else {
+    resetPyramid();
+    console.log("pyramid reset");
+  }
 };
